4fc70228eb89cb5fa1e83f8d4e26ff5b6b4e9b63
Matthew McConnell <2235757M@student.gla.ac.uk>
2018-01-18 18:55:41 +0000

    Finished chapter 9 and fixed all file merge conflicts in the last commit FYI

--- rango/admin.py
+++ rango/admin.py
@@ -4,12 +4,9 @@ from rango.models import Category, Page
 class PageAdmin (admin.ModelAdmin):
     list_display = ("title", "category", "url")
 
-<<<<<<< HEAD
+
 class CategoryAdmin (admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 admin.site.register(Category, CategoryAdmin)
-=======
-admin.site.register(Category)
->>>>>>> eaa5e06... Added a page admin class to change the look of the admin page about pages to look like a table, also ran the population script
 admin.site.register(Page, PageAdmin)
